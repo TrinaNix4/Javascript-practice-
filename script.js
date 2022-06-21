78e3ff54999fcb43332fe6f1c6503487cc600222
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-06-21 15:29:53 -0600

    interpolation practice

--- script.js
+++ script.js
@@ -31,27 +31,42 @@
 // console.log(ageTrina > ageSarah);
 // //>, <, >=, <=
 
-const markHeight1 = 1.69;
-const markMass1 = 78;
+// const markHeight1 = 1.69;
+// const markMass1 = 78;
 
-const johnHeight1 = 1.95;
-const johnMass1 = 92;
+// const johnHeight1 = 1.95;
+// const johnMass1 = 92;
 
-const markHeight2 = 1.88;
-const markMass2 = 95;
+// const markHeight2 = 1.88;
+// const markMass2 = 95;
 
-const johnHeight2 = 1.76;
-const johnMass2 = 85;
+// const johnHeight2 = 1.76;
+// const johnMass2 = 85;
 
-const markBMI1 = markMass1 / (markHeight1 * markHeight1);
+// const markBMI1 = markMass1 / (markHeight1 * markHeight1);
 
-const johnBMI1 = johnMass1 / (johnHeight1 * johnHeight1);
+// const johnBMI1 = johnMass1 / (johnHeight1 * johnHeight1);
 
-const markBMI2 = markMass2 / (markHeight2 * markHeight2);
+// const markBMI2 = markMass2 / (markHeight2 * markHeight2);
 
-const johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
+// const johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
 
-console.log(markBMI1);
-console.log(johnBMI1);
-console.log(markBMI2);
-console.log(johnBMI2);
+// console.log(markBMI1);
+// console.log(johnBMI1);
+// console.log(markBMI2);
+// console.log(johnBMI2);
+
+const firstName = "Trina";
+const job = "student";
+const birthYear = 1982;
+const year = 2022;
+
+const trina =
+  "I'm " + firstName + ", a" + (year - birthYear) + "years old" + job + "!";
+console.log(trina);
+
+//template literal make the above easier
+//interpolation
+
+const trinaNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(trinaNew);
