a06aa7fc0b848da95e51fea9677aa4f9916bc203
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-06-21 15:06:34 -0600

    init file setup linked script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+// let js = "amazing";
+// if (js === "amazing") alert("hello!");
+// console.log(40 + 8);
+
+// let firstName = "trina";
+// console.log(firstName);
+
+// let country = "USA";
+// let continent = "North America";
+// let population = "380 million";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// const currentYear = 2022;
+// const ageTrina = currentYear - 1982;
+// console.log(ageTrina * 2, ageTrina / 2);
+// console.log(firstName + country);
+
+// const ageSarah = now - 2014;
+// //assignment operators
+// let x = 10;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+
+// //comparison operators
+// console.log(ageTrina > ageSarah);
+// //>, <, >=, <=
+
+const markHeight1 = 1.69;
+const markMass1 = 78;
+
+const johnHeight1 = 1.95;
+const johnMass1 = 92;
+
+const markHeight2 = 1.88;
+const markMass2 = 95;
+
+const johnHeight2 = 1.76;
+const johnMass2 = 85;
+
+const markBMI1 = markMass1 / (markHeight1 * markHeight1);
+
+const johnBMI1 = johnMass1 / (johnHeight1 * johnHeight1);
+
+const markBMI2 = markMass2 / (markHeight2 * markHeight2);
+
+const johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
+
+console.log(markBMI1);
+console.log(johnBMI1);
+console.log(markBMI2);
+console.log(johnBMI2);
